190cdb9829b4339b18259a76f0f5aff81d9cde2a
Andrews Medina <andrewsmedina@gmail.com>
2016-10-17 14:25:51 -0200

    bump to 0.6.1

--- hm/__init__.py
+++ hm/__init__.py
@@ -1,5 +1,5 @@
-# Copyright 2015 hm authors. All rights reserved.
+# Copyright 2016 hm authors. All rights reserved.
 # Use of this source code is governed by a BSD-style
 # license that can be found in the LICENSE file.
 
-__version__ = '0.6.0'
+__version__ = '0.6.1'
